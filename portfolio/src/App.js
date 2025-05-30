871d6816f93c32a167eb8a9d6033cb5694b992f2
Sneha Sundar <snehas9@illinois.edu>
2025-05-30 17:25:33 -0500

    everything except the resume update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { ReactTyped } from "react-typed";
+import { FaGithub, FaLinkedin, FaEnvelope} from "react-icons/fa";
+import { DiGoogleCloudPlatform } from "react-icons/di";
+import { SiPytorch, SiMongodb} from "react-icons/si";
+import { SiJupyter } from "react-icons/si";
+
 
 import {
   SiDjango,
@@ -26,10 +31,12 @@ import {
   FaGitAlt,
 } from "react-icons/fa";
 
+
+
 import "./App.css";
 
 // prof pic
-import profilePicture from "./images/profile-picture.jpeg";
+import profilePicture from "./images/profile-picture.png";
 
 // project images
 import restaurantReccomender from "./images/restaurant-recommender.png";
@@ -41,140 +48,124 @@ import generatedFaces from "./images/generated-faces.png";
 import resume from "./files/sneha_sundar_cv.pdf";
 
 function App() {
-  const [currentCard, setCurrentCard] = useState(0);
-  const cards = [
-    {
-      content:
-        "I'm pursuing a Bachelor of Science in Statistics & Computer Science at the University of Illinois Urbana-Champaign.",
-    },
-    {
-      content:
-        "I'm currently working with Dr. Tal August at the Language Interaction Lab at UIUC, using natural language processing to analyze and quantify certainty levels in academic citations!",
-    },
-    {
-      content:
-        "Last summer, I was a SWE intern at Arrcus on their Customer Solutions Engineering Team.",
-    },
-  ];
-
+  
   const techStack = [
     {
       name: "JavaScript",
       type: "language",
-      icon: <FaJsSquare />,
+      icon: <FaJsSquare size={28} color="#F7DF1E" />,
       url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       name: "HTML",
       type: "language",
-      icon: <FaHtml5 />,
+      icon: <FaHtml5 size={28} color="#E34F26" />,
       url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
     },
     {
       name: "CSS",
       type: "language",
-      icon: <FaCss3Alt />,
+      icon: <FaCss3Alt size={28} color="#1572B6" />,
       url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     },
     {
       name: "Python",
       type: "language",
-      icon: <FaPython />,
+      icon: <FaPython size={28} color="#3776AB" />,
       url: "https://www.python.org/doc/",
     },
     {
       name: "Java",
       type: "language",
-      icon: <FaJava />,
+      icon: <FaJava size={28} color="#007396" />,
       url: "https://docs.oracle.com/en/java/",
     },
     {
       name: "R",
       type: "language",
-      icon: <FaRProject />,
+      icon: <FaRProject size={28} color="#276DC3" />,
       url: "https://www.rdocumentation.org/",
     },
     {
       name: "PyTorch",
       type: "technology",
-      icon: <FaFire />,
+      icon: <FaFire size={28} color="#EE4C2C" />,
       url: "https://pytorch.org/docs/stable/index.html",
     },
     {
       name: "NumPy",
       type: "technology",
-      icon: <SiNumpy />,
+      icon: <SiNumpy size={28} color="#013243" />,
       url: "https://numpy.org/doc/",
     },
     {
       name: "TypeScript",
       type: "language",
-      icon: <SiTypescript />,
+      icon: <SiTypescript size={28} color="#3178C6" />,
       url: "https://www.typescriptlang.org/docs/",
     },
     {
       name: "C++",
       type: "language",
-      icon: <CgCPlusPlus />,
+      icon: <CgCPlusPlus size={28} color="#00599C" />,
       url: "https://en.cppreference.com/w/cpp",
     },
     {
       name: "SQL",
       type: "language",
-      icon: <SiMysql />,
+      icon: <SiMysql size={28} color="#4479A1" />,
       url: "https://learn.microsoft.com/en-us/sql/?view=sql-server-ver16",
     },
-    
     {
       name: "React",
       type: "framework",
-      icon: <FaReact />,
+      icon: <FaReact size={28} color="#61DAFB" />,
       url: "https://reactjs.org/docs/getting-started.html",
     },
     {
       name: "Django",
       type: "framework",
-      icon: <SiDjango />,
+      icon: <SiDjango size={28} color="#092E20" />,
       url: "https://docs.djangoproject.com/en/stable/",
     },
     {
       name: "Git",
       type: "technology",
-      icon: <FaGitAlt />,
+      icon: <FaGitAlt size={28} color="#F05032" />,
       url: "https://git-scm.com/doc",
     },
     {
       name: "TensorFlow",
       type: "technology",
-      icon: <SiTensorflow />,
+      icon: <SiTensorflow size={28} color="#FF6F00" />,
       url: "https://www.tensorflow.org/guide",
     },
     {
       name: "Keras",
       type: "technology",
-      icon: <SiKeras />,
+      icon: <SiKeras size={28} color="#D00000" />,
       url: "https://keras.io/",
     },
     {
       name: "Flask",
       type: "framework",
-      icon: <SiFlask />,
+      icon: <SiFlask size={28} color="#000000" />,
       url: "https://flask.palletsprojects.com/en/2.0.x/",
     },
     {
       name: "Pandas",
       type: "technology",
-      icon: <SiPandas />,
+      icon: <SiPandas size={28} color="#150458" />,
       url: "https://pandas.pydata.org/docs/",
     },
     {
       name: "VSCode",
       type: "technology",
-      icon: <FaCode />,
+      icon: <FaCode size={28} color="#007ACC" />,
       url: "https://code.visualstudio.com/docs",
     },
   ];
-
+  
   const coursework = [
     {
       name: "Data Structures",
@@ -229,13 +220,51 @@ function App() {
       name: "Computational Social Science",
       number: "CS 498"
     }
-  ];
+  ]; 
 
   const projects = [
     {
       title: "Face Generator",
       description: "Built a GAN to generate faces based on the CelebA dataset.",
-      techUsed: ["PyTorch", "torchvision", "Matplotlib"],
+      techUsed: ["PyTorch", "Matplotlib", "Python", "Jupyter"],
+      techUsedIcons: [
+        <a
+          key="pytorch"
+          href="https://pytorch.org/docs/stable/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiPytorch size={28} color="#EE4C2C" />
+        </a>,
+        <a
+          key="matplotlib"
+          href="https://matplotlib.org/stable/contents.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/matplotlib.png"
+            alt="Matplotlib"
+            style={{ width: '2em', height: '2em' }}
+          />
+        </a>,
+        <a
+          key="python"
+          href="https://www.python.org/doc/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaPython size={28} color="#3776AB" />
+        </a>,
+        <a
+          key="jupyter"
+          href="https://jupyter.org/documentation"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiJupyter size={28} color="#F37626" />
+        </a>,
+      ],
       link: "https://github.com/snehasund/generated-faces",
       image: generatedFaces,
     },
@@ -243,7 +272,57 @@ function App() {
       title: "Restaurant Recommender",
       description:
         "Created a full-stack web application to help users simplify the decision of where to eat. This application was built with KMeans clustering to find the most highly regarded restaurants based on the user's cuisine preferences using the Geolocate API and Google Places API.",
-      techUsed: ["React.js", "Flask", "TypeScript", "MongoDB", "Google OAuth"],
+      techUsed: ["Python", "React", "Flask", "TypeScript", "MongoDB", "Google Cloud"],
+      techUsedIcons: [
+        <a
+          key="python"
+          href="https://www.python.org/doc/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaPython size={28} color="#3776AB" />
+        </a>,
+        <a
+          key="react"
+          href="https://reactjs.org/docs/getting-started.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaReact size={28} color="#61DAFB" />
+        </a>,
+        <a
+          key="flask"
+          href="https://flask.palletsprojects.com/en/2.0.x/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiFlask size={28} color="#000000" />
+        </a>,
+        <a
+          key="typescript"
+          href="https://www.typescriptlang.org/docs/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiTypescript size={28} color="#3178C6" />
+        </a>,
+        <a
+          key="mongodb"
+          href="https://docs.mongodb.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiMongodb size={28} color="#47A248" />
+        </a>,
+        <a
+          key="google-cloud"
+          href="https://cloud.google.com/docs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <DiGoogleCloudPlatform size={28} color="#4285F4" />
+        </a>,
+      ],
       link: "https://github.com/snehasund/restaurant-recommender",
       image: restaurantReccomender,
     },
@@ -251,7 +330,37 @@ function App() {
       title: "World Happiness Visualization",
       description:
         "Utilizing R, conducted data cleaning and preparation for the World Happiness Report dataset. Visualized happiness scores and their distribution across regions using bar charts and density curves created with ggplot2, effectively communicating key insights through visualizations.",
-      techUsed: ["RStudio"],
+      techUsed: ["RStudio", "React", "CSS"],
+      techUsedIcons: [
+        <a
+          key="rstudio"
+          href="https://posit.co/download/rstudio-desktop/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/rstudio.png"
+            alt="RStudio"
+            style={{ width: '2em', height: '2em' }}
+          />
+        </a>,
+        <a
+          key="react"
+          href="https://reactjs.org/docs/getting-started.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaReact size={28} color="#61DAFB" />
+        </a>,
+        <a
+          key="css"
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaCss3Alt size={28} color="#1572B6" />
+        </a>,
+      ],
       link: "https://github.com/snehasund/world-happiness-visualization",
       image: worldHappinessVisualization,
     },
@@ -259,11 +368,58 @@ function App() {
       title: "Long Texts & Websites Summarizer",
       description:
         "Summarizes web-pages (like wikipedia sites) & long texts to save users' time and provide them with concise information.",
-      techUsed: ["Flask", "BeautifulSoup", "Natural Language Toolkit (NLTK)"],
+      techUsed: ["Flask", "NLTK", "Python", "HTML", "CSS"],
+      techUsedIcons: [
+        <a
+          key="flask"
+          href="https://flask.palletsprojects.com/en/2.0.x/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiFlask size={28} color="#000000" />
+        </a>,
+        <a
+          key="nltk"
+          href="https://www.nltk.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/nltk.png"
+            alt="NLTK"
+            style={{ width: '2.6em', height: '2.6em' }}
+          />
+        </a>,
+        <a
+          key="python"
+          href="https://www.python.org/doc/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaPython size={28} color="#3776AB" />
+        </a>,
+        <a
+          key="html"
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaHtml5 size={28} color="#E34F26" />
+        </a>,
+        <a
+          key="css"
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaCss3Alt size={28} color="#1572B6" />
+        </a>,
+      ],
       link: "https://github.com/snehasund/summarizer",
       image: summarizer,
     },
   ];
+  
 
   const pastProjects = [
     {
@@ -284,38 +440,17 @@ function App() {
     },
   ];
 
-  const nextCard = () => {
-    setCurrentCard((currentCard + 1) % cards.length);
-  };
-
-  const prevCard = () => {
-    setCurrentCard((currentCard - 1 + cards.length) % cards.length);
-  };
-
   return (
     <div className="App">
-      <div className="navbar">
+      <nav className="navbar">
         <div className="nav-name">Sneha Sundar</div>
-        <div class="nav-icons">
-          <a
-            href="https://www.linkedin.com/in/sneha-sundar26"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-linkedin white-icon larger-icon"></i>
-          </a>
-          <a href="mailto:snehas9@illinois.edu">
-            <i className="far fa-envelope white-icon larger-icon"></i>
-          </a>
-          <a
-            href="https://github.com/snehasund"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github white-icon larger-icon"></i>
-          </a>
+        <div className="nav-links">
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="mailto:snehas9@illinois.edu">Contact Me</a>
         </div>
-      </div>
+      </nav>
+
 
       <header className="App-header">
         <div className="intro">
@@ -341,29 +476,30 @@ function App() {
               </h1>
             </div>
             <h2>
-              Hi, I'm Sneha! I am a Statistics and Computer Science Major at the
-              University of Illinois at Urbana-Champaign, graduating in May
-              2026. I am currently working with{" "}
-              <a style={{color: 'darkgreen'}} href="https://talaugust.github.io/" target="_blank">
-                Dr. Tal August
-              </a>{" "}
-              at the Language Interaction Lab, using natural language processing
-              to analyze and quantify certainty levels in academic citations.
-              This research aims to enable comparative confidence analysis
-              across different research domains.
+              Hi, I'm Sneha! I am a Statistics and Computer Science Major at the  
+              University of Illinois at Urbana-Champaign. I am currently working on an  
+              NSF-funded project in data-centric privacy at the University of Houston with{' '}
+              <a style={{color: "darkgreen", fontWeight: "bold"}} href="https://www.ece.uh.edu/faculty/leiss" target="_blank" rel="noopener noreferrer">
+                 Dr. Ernst Leiss
+              </a>.
             </h2>
+
 
             <div className="button-container">
               <a href={resume} download className="download-btn">
-                Resume
-              </a>
+          <img
+            src="/resume.png"
+            alt="resume"
+            style={{ width: '2.2em', height: '2.2em'}}
+          />
+        </a>
               <a
                 href="https://github.com/snehasund"
                 className="download-btn"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Github
+                <FaGithub size={32} color="#171515" />
               </a>
               <a
                 href="https://linkedin.com/in/ssundar11"
@@ -371,10 +507,10 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                LinkedIn
+                <FaLinkedin size={32} color="#0A66C2" />
               </a>
-              <a href="mailto:snehamsundar@gmail.com" className="download-btn">
-                Email
+              <a href="mailto:snehas9@illinois.edu" className="download-btn">
+              <FaEnvelope size={32} color="#D44638" />
               </a>
             </div>
           </div>
@@ -417,7 +553,13 @@ function App() {
                 </h3>
                 <p>{project.description}</p>
                 <p>
-                  <strong>Tech used:</strong> {project.techUsed.join(", ")}
+                <p className="tech-icons">
+                    {project.techUsedIcons.map((icon, i) => (
+                      <span key={i} className="tech-icon">
+                        {icon}
+                      </span>
+                    ))}
+                </p>
                 </p>
               </div>
               <img
@@ -428,7 +570,7 @@ function App() {
             </div>
           ))}
 
-          {/* New card for past projects */}
+          {/* past projects */}
           <div className="project-card">
             <div className="card-content">
               <h3>More Projects</h3>
@@ -451,16 +593,15 @@ function App() {
         </div>
       </section>
 
-      {/* Tech Stack Section */}
+      {/* tech stack */}
       <section className="section">
         <h2>My Most-Used Tech!</h2>
         <div class="boxes">
-          {/* Languages Card */}
           <div className="tech-card">
             <h3>Programming Languages</h3>
             <div className="tech-stack languages">
               {techStack
-                .filter((tech) => tech.type === "language") // Adjust the type filter as needed ("language", "framework", etc.)
+                .filter((tech) => tech.type === "language")
                 .map((tech, index) => (
                   <div key={index} className="tech-item">
                     <a
@@ -472,7 +613,6 @@ function App() {
                       <div style={{ textAlign: "center" }}>
                         {React.cloneElement(tech.icon, {
                           size: 32,
-                          color: "#011f14",
                         })}
                       </div>
                       <div style={{ textAlign: "center" }}>{tech.name}</div>
@@ -482,7 +622,6 @@ function App() {
             </div>
           </div>
 
-          {/* Frameworks and Technologies Card */}
           <div className="tech-card">
             <h3>Frameworks/Technologies</h3>
             <div className="tech-stack frameworks">
@@ -499,7 +638,6 @@ function App() {
                       <div style={{ textAlign: "center" }}>
                         {React.cloneElement(tech.icon, {
                           size: 32,
-                          color: "#011f14",
                         })}
                       </div>
                       <div style={{ textAlign: "center" }}>{tech.name}</div>
@@ -529,27 +667,29 @@ function App() {
 
       {/* footer */}
       <footer className="App-footer">
-        <p>Connect with me!</p>
-        <div className="social-icons">
-          <a
-            href="https://www.linkedin.com/in/sneha-sundar26"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-linkedin black-icon larger-icon"></i>
-          </a>
-          <a href="mailto:snehamsundar@gmail.com">
-            <i className="far fa-envelope black-icon larger-icon"></i>
-          </a>
-          <a
-            href="https://github.com/snehasund"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github black-icon larger-icon"></i>
-          </a>
-        </div>
-      </footer>
+  <p>Connect with me!</p>
+  <div className="social-icons">
+    <a
+      href="https://www.linkedin.com/in/ssundar11"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="LinkedIn"
+    >
+      <FaLinkedin size={32} color="#0A66C2" />
+    </a>
+    <a href="mailto:snehas9@illinois.edu" aria-label="Email">
+      <FaEnvelope size={32} color="#D44638" />
+    </a>
+    <a
+      href="https://github.com/snehasund"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="GitHub"
+    >
+      <FaGithub size={32} color="#171515" />
+    </a>
+  </div>
+</footer>
     </div>
   );
 }
